e17cc7d24a991c52a58c3204df9d6cec04310290
Artem Iashin <artem_iashin@epam.com>
2025-06-02 17:32:12 +0600

    New plugins

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -4,5 +4,13 @@ return {
 	    config = function()
 		    require("nvim-ts-autotag").setup()
 	    end
+	},
+	{
+	    'altermo/ultimate-autopair.nvim',
+	    event={'InsertEnter','CmdlineEnter'},
+	    branch='v0.6', --recommended as each new version will have breaking changes
+	    opts={
+		--Config goes here
+	    },
 	}
 }
