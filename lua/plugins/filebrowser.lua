ab3646cd787528d413401d0f13f7e580e8d50719
Artem Iashin <artem_iashin@epam.com>
2025-08-04 04:21:35 +0600

    updated file browser

--- lua/plugins/filebrowser.lua
+++ lua/plugins/filebrowser.lua
@@ -1,26 +1,70 @@
-return {
-	{
-		"nvim-tree/nvim-tree.lua",
-		version = "*",
-		lazy = false,
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
-		config = function()
-			require("nvim-tree").setup({
-				view = {
-					width = {
-						min = 30,
-						max = -1,
-					}
-				},
-				update_focused_file = {
-					enable = true
-				}
-			})
-			vim.keymap.set("n", "<leader>d", ":NvimTreeToggle<CR>", {silent = true, noremap = true})
-		end
-	}
-}
+-- return {
+-- 	{
+-- 		"nvim-tree/nvim-tree.lua",
+-- 		version = "*",
+-- 		lazy = false,
+-- 		dependencies = {
+-- 			"nvim-tree/nvim-web-devicons",
+-- 		},
+-- 		config = function()
+-- 			require("nvim-tree").setup({
+-- 				view = {
+-- 					width = {
+-- 						min = 30,
+-- 						max = -1,
+-- 					}
+-- 				},
+-- 				update_focused_file = {
+-- 					enable = true
+-- 				}
+-- 			})
+-- 			vim.keymap.set("n", "<leader>d", ":NvimTreeToggle<CR>", {silent = true, noremap = true})
+-- 		end
+-- 	}
+-- }
 
--- return {}
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons", -- optional, but recommended
+    },
+    lazy = false, -- neo-tree will lazily load itself
+    config = function()
+	    require('neo-tree').setup({
+		    window = {
+			    auto_expand_width = true,
+			    mappings = {
+				    ['o'] = 'open',
+				    ["oc"] = false,
+					["od"] = false,
+					["og"] = false,
+					["om"] = false,
+					["on"] = false,
+					["os"] = false,
+					["ot"] = false,
+			    }
+		    },
+		    filesystem = {
+			    filtered_items = {
+				    hide_dotfiles = false,
+				    hide_gitignored = false
+			    },
+			    follow_current_file = {
+				    enabled = true,
+				    leave_dirs_open = false,
+			    }
+		    },
+		    buffers = {
+			    follow_current_file = {
+				    enabled = true
+			    },
+		    }
+	    })
+	    vim.api.nvim_set_keymap('n', '<leader>d' ,':Neotree toggle<CR>', {noremap = true, silent = true})
+    end
+  }
+}
